7880cc34d4977d02fc98d8164e886b3a60298ae1
Alejandro <alejandro.dominguez-lopez@iesruizgijon.com>
2024-06-13 02:35:47 +0200

    commit api middleware

--- proyectoTFG/server/routes/api.php
+++ proyectoTFG/server/routes/api.php
@@ -11,43 +11,43 @@ use Illuminate\Support\Facades\Route;
 // })->name('login');
 
 // Ruta registro cliente
-// Route::middleware(['api'])->group(function () {
-// Ruta registro cliente
-Route::post('/registroCliente', [ClienteController::class, 'guardarCliente'])->name('registroCliente');
+Route::middleware(['api'])->group(function () {
+    // Ruta registro cliente
+    Route::post('/registroCliente', [ClienteController::class, 'guardarCliente'])->name('registroCliente');
 
-// Ruta login cliente
-Route::post('/loginCliente', [ClienteController::class, 'comprobarLogin'])->name('loginCliente');
+    // Ruta login cliente
+    Route::post('/loginCliente', [ClienteController::class, 'comprobarLogin'])->name('loginCliente');
 
-// Comprobar Session Login
-Route::get('/comprobarSesion', [ClienteController::class, 'comprobarSesion'])->name('comprobarSesion');
+    // Comprobar Session Login
+    Route::get('/comprobarSesion', [ClienteController::class, 'comprobarSesion'])->name('comprobarSesion');
 
-// Buscar vuelo
-Route::post('/buscarVuelo', [VueloController::class, 'buscarVuelo'])->name('buscarVuelo');
+    // Buscar vuelo
+    Route::post('/buscarVuelo', [VueloController::class, 'buscarVuelo'])->name('buscarVuelo');
 
-// Obtener Cliente
-Route::post('/obtenerCliente', [ClienteController::class, 'obtenerCliente'])->name('obtenerCliente');
+    // Obtener Cliente
+    Route::post('/obtenerCliente', [ClienteController::class, 'obtenerCliente'])->name('obtenerCliente');
 
-// Pagar vuelo
-Route::post('/pagarVuelo', [VueloController::class, 'pagarVuelo'])->name('pagarVuelo');
+    // Pagar vuelo
+    Route::post('/pagarVuelo', [VueloController::class, 'pagarVuelo'])->name('pagarVuelo');
 
-// Guardar vuelo
-Route::post('/guardarVueloBillete', [VueloController::class, 'guardarVueloBillete'])->name('guardarVueloBillete');
+    // Guardar vuelo
+    Route::post('/guardarVueloBillete', [VueloController::class, 'guardarVueloBillete'])->name('guardarVueloBillete');
 
-// Obtener billetes
-Route::post('/obtenerBilletes', [BilleteController::class, 'obtenerBilletes'])->name('obtenerBilletes');
+    // Obtener billetes
+    Route::post('/obtenerBilletes', [BilleteController::class, 'obtenerBilletes'])->name('obtenerBilletes');
 
-// Obtener vuelo
-Route::post('/obtenerVuelo', [VueloController::class, 'obtenerVuelo'])->name('obtenerVuelo');
+    // Obtener vuelo
+    Route::post('/obtenerVuelo', [VueloController::class, 'obtenerVuelo'])->name('obtenerVuelo');
 
-// Cancelar billete
-Route::post('/eliminarBillete', [BilleteController::class, 'eliminarBillete'])->name('eliminarBillete');
+    // Cancelar billete
+    Route::post('/eliminarBillete', [BilleteController::class, 'eliminarBillete'])->name('eliminarBillete');
 
-// Rceuperar contraseña
-Route::post('/recuperarContrasena', [ClienteController::class, 'recuperarContrasena'])->name('recuperarContrasena');
+    // Rceuperar contraseña
+    Route::post('/recuperarContrasena', [ClienteController::class, 'recuperarContrasena'])->name('recuperarContrasena');
 
-// Comprobar asientos
-Route::post('/comprobarAsientos', [BilleteController::class, 'comprobarAsientos'])->name('comprobarAsientos');
-// });
+    // Comprobar asientos
+    Route::post('/comprobarAsientos', [BilleteController::class, 'comprobarAsientos'])->name('comprobarAsientos');
+});
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:api');
